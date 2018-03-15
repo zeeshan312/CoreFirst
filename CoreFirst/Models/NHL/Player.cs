3de459d3cb096b3a7e9d7b52df62c5a4e27a98d1
whinstone1 <zeemurrawat@hotmail.com>
2018-03-15 10:52:49 +0500

    modelclasses

---
+++ CoreFirst/Models/NHL/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoreFirstPrimer.Models.NHL
+{
+    public class Player
+    {
+
+        public int PlayerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+
+
+
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+
+    }
+}
+
+
+
+
---
+++ CoreFirst/Models/NHL/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoreFirstPrimer.Models.NHL
+{
+    public class Team
+    {
+        [Key]
+        [MaxLength(30)]
+        public int TeamID { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+
+
+        public List<Player> Players { get; set; }
+        public int country { get; set; }
+        public Country countries { get; set; }
+    }
+}
